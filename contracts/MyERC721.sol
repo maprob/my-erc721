97b5a93b806018c9ab0004b89c1530d470be8fc7
jocko willi <jockowilliisbadass@yahoo.com>
2022-08-12 13:39:22 +0300

    added test scoopes and uri to nft

--- contracts/MyERC721.sol
+++ contracts/MyERC721.sol
@@ -4,48 +4,62 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract MyERC721 is ERC721Enumerable, ERC721URIStorage, Ownable {
 
-    uint256 public tokenCounter;
-    uint256 public maxSupply;
+    string baseURI;
+    string public baseExtension = ".json";
+    uint256 public tokenCounter = 0;
+    uint256 public maxSupply = 50;
+    uint256 public cost = 0.05 ether;
 
     mapping(uint256 => address) public owners;
     mapping(uint256 => string) private _tokenURIs;
 
     event CreatedNFT(uint256 indexed tokenId, string tokenURI);
 
-    constructor() ERC721("myNFT", "MNFT")
+    constructor (
+        string memory _name,
+        string memory _symbol,
+        string memory _initBaseURI
+    )
+    ERC721(_name, _symbol)
     {
-        tokenCounter = 0;
-        maxSupply = 1000;
+        baseURI = _initBaseURI;
     }
 
     function create() public payable
     {
         require (totalSupply() < maxSupply);
-        require(msg.value == 0.02*10**18);
+        require(msg.value == cost);
 
-        string memory imageURI = getImageURI();
-        string memory _tokenURI = formatTokenURI(imageURI);
+        string memory _tokenURI = tokenURI(tokenCounter);
 
         _safeMint(msg.sender, tokenCounter);
         _setTokenURI(tokenCounter, _tokenURI);
         emit CreatedNFT(tokenCounter, _tokenURI);
 
         owners[tokenCounter] = msg.sender;
-
         tokenCounter += 1;
     }
 
-    function getImageURI() public pure returns (string memory)
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
     {
-        return string("null");
-    }
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
 
-    function formatTokenURI(string memory imageURI) public pure returns (string memory)
-    {
-        return string(imageURI);
+        string memory currentBaseURI = _baseURI();
+        return bytes(currentBaseURI).length > 0
+            ? string(abi.encodePacked(currentBaseURI, Strings.toString(tokenCounter), baseExtension))
+            : "";
     }
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId)
@@ -59,15 +73,6 @@ contract MyERC721 is ERC721Enumerable, ERC721URIStorage, Ownable {
         super._burn(tokenId);
     }
 
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
-    }
-
     function supportsInterface(bytes4 interfaceId)
         public
         view
@@ -77,8 +82,15 @@ contract MyERC721 is ERC721Enumerable, ERC721URIStorage, Ownable {
         return super.supportsInterface(interfaceId);
     }
 
-    function _baseURI() internal pure override returns (string memory) {
-        return "";
+    function _baseURI() internal view virtual override returns (string memory)
+    {
+        return baseURI;
+    }
+
+    function withdraw() public payable onlyOwner
+    {
+        (bool os, ) = payable(owner()).call{value: address(this).balance}("");
+        require(os);
     }
 
 }
--- scripts/myERC721/1_deploy_nft_factory.py
+++ scripts/myERC721/1_deploy_nft_factory.py
@@ -2,7 +2,8 @@ from brownie import accounts, config, MyERC721
 
 def main():
     acc = accounts.add(config["wallets"]["from_key"])
+    baseUri = ""
     erc721 = MyERC721.deploy(
-        {"from": acc}, publish_source=True
+        baseUri, {"from": acc}, publish_source=True
     )
     return erc721
